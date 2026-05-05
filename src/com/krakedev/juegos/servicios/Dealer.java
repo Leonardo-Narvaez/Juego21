b1e389466b2e474b614a7c2c0b11c70a4537078a
Leonardo-Narvaez <leonardonarvaez12@gmail.com>
2026-05-04 23:41:45 -0500

    LNMQ: se crea la clase Dealer, con getters, setters, constructor y los metodos generarNaipe, imprimirNaipe, generarAleatorio

---
+++ src/com/krakedev/juegos/servicios/Dealer.java
@@ -0,0 +1,47 @@
+package com.krakedev.juegos.servicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.krakedev.juegos.entidades.Carta;
+
+public class Dealer {
+	private ArrayList<Carta> baraja;
+
+	public ArrayList<Carta> getBaraja() {
+		return baraja;
+	}
+
+	public void setBaraja(ArrayList<Carta> baraja) {
+		this.baraja = baraja;
+	}
+
+	public Dealer() {
+		baraja = new ArrayList<>();
+		generarNaipe();
+
+	}
+
+	public void generarNaipe() {
+		ArrayList<String> palo = new ArrayList<>(List.of("T", "CN", "CR", "D"));
+		ArrayList<String> valor = new ArrayList<>(
+				List.of("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"));
+		for (String simbolo : palo) {
+			for (String valor1 : valor) {
+				baraja.add(new Carta(valor1, simbolo));
+			}
+
+		}
+	}
+
+	public void imprimirNaipe() {
+		for (Carta naipe : baraja) {
+			naipe.imprimir();
+		}
+	}
+
+	public int generarAleatorio(int maximo) {
+		int numerico = (int) (Math.random() * (maximo + 1));
+		return numerico;
+	}
+}
